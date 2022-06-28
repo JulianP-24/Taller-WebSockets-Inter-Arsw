5ed61dff4d8b88eb597c6d1b3266381982c1d7be
unknown <julian.pena-m@labinfo.is.escuelaing.edu.co>
2022-06-28 09:50:04 -0500

    first commit

---
+++ src/main/java/edu/escuelaing/arsw/App/Components/TimedMessageBroker.java
@@ -0,0 +1,23 @@
+package edu.escuelaing.arsw.App.Components;
+
+import edu.escuelaing.arsw.App.EndPoint.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.context.annotation.Scope;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class TimedMessageBroker {
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+    private static final Logger logger = Logger.getLogger(TimedMessageBroker.class.getName());
+
+    @Scheduled(fixedRate = 5000)
+    public void broadcast() {
+        logger.log(Level.INFO, "broadcastingMessages");
+        TimerEndPoint.send("The time is now " + dateFormat.format(new Date()));
+    }
+}
---
+++ src/main/java/edu/escuelaing/arsw/App/WsStartAppApplication.java
@@ -0,0 +1,13 @@
+package edu.escuelaing.arsw.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsStartAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsStartAppApplication.class, args);
+	}
+
+}
